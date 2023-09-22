81b2de8f5e17f5d39dcd215d4e9dc005d5d0a525
Khale Coffran <khalecoffran@gmail.com>
2023-09-22 14:28:40 -0400

    Added replayablity and cheat detection

--- index.js
+++ index.js
@@ -10,41 +10,55 @@ function ask(questionText) {
 start();
 
 async function start() {
-  console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
-  let lowestNum = await ask("What is the lowest number in the range? ");
-  let highestNum = await ask("What is the highest number in the range? ");
-  let secretNumber = await ask("What is your secret number?\n Whole numbers only.\nI won't peek, I promise...\n");
-  console.log('You entered: ' + secretNumber);
+  let playAgain = "y";
+  while (playAgain.toLowerCase() == "y"){
+    console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
+    let lowestNum = await ask("What is the lowest number in the range? ");
+    let highestNum = await ask("What is the highest number in the range? ");
+    let secretNumber = await ask("What is your secret number?\nWhole numbers only.\nI won't peek, I promise...\n");
+    console.log('You entered: ' + secretNumber);
 
-  lowestNum = Number(lowestNum);
-  highestNum = Number(highestNum);
-  secretNumber = Number(secretNumber);
-  
-  if (secretNumber > highestNum || secretNumber < lowestNum){
-    process.stdout.write("No fair! Your trying to Cheat!");
-    rl.close();
-  } else if (isNaN(secretNumber)) {
-    process.stdout.write("That's not a number.");
-    rl.close();
-  } else {
-    process.stdout.write("Lets begin!");
-    let numGuessed = "n";
-    let tries = 1;
-    while (numGuessed.toLowerCase() != "y") {
-      numGuessed = await ask(`Is your Number ${Math.floor((highestNum + lowestNum) / 2)}? (Y/N)? `);
-      if (numGuessed.toLowerCase() == "n") {
-        tries++;
-        let isNum = await ask("Is your Number higher or lower? (H/L)? ");
-        if (isNum.toLowerCase() == "h"){
-          lowestNum = Math.floor((highestNum + lowestNum) / 2);
-        } else if (isNum.toLowerCase() == "l") {
-          highestNum = Math.floor((highestNum + lowestNum) / 2);
+    lowestNum = Number(lowestNum);
+    highestNum = Number(highestNum);
+    secretNumber = Number(secretNumber);
+    
+    if (secretNumber > highestNum || secretNumber < lowestNum){
+      process.stdout.write("No fair! Your trying to Cheat!");
+      process.exit();
+    } else if (isNaN(secretNumber)) {
+      process.stdout.write("That's not a number.");
+      process.exit();
+    } else {
+      process.stdout.write("Lets begin!");
+      let numGuessed = "n";
+      let tries = 1;
+      while (numGuessed.toLowerCase() != "y") {
+        numGuessed = await ask(`Is your Number ${Math.floor((highestNum + lowestNum) / 2)}? (Y/N)? `);
+        if (numGuessed.toLowerCase() == "n") {
+          var cheated = false;
+          tries++;
+          if (prevGuess != (Math.floor((highestNum + lowestNum) / 2))){
+            var prevGuess = 0;
+            let isNum = await ask("Is your Number higher or lower? (H/L)? ");
+            if (isNum.toLowerCase() == "h"){
+              lowestNum = Math.floor((highestNum + lowestNum) / 2);
+              prevGuess = lowestNum;
+            } else if (isNum.toLowerCase() == "l") {
+              highestNum = Math.floor((highestNum + lowestNum) / 2);
+              prevGuess = highestNum;
+            }
+          } else {
+            console.log(`Your number can only be ${prevGuess} baised on what you told me. You have lied, thou art a villan and cur.`);
+            numGuessed = "y";
+            cheated = true;
+          }
+        } else if (cheated == false && numGuessed.toLowerCase() == "y"){
+          console.log(`Hurray, I got it in ${tries} tries! Your number was ${Math.floor((highestNum + lowestNum) / 2)}.`);
         }
-      } else {
-        process.stdout.write(`Hurray, I got it in ${tries} tries! Your number was ${Math.floor((highestNum + lowestNum) / 2)}.`);
       }
     }
-  }
 
-  process.exit();
+    playAgain = await ask("Would you like to play again? (Y/N) ");
+}
+process.exit();
 }
