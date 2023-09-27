62cf72fe4dcadf2d2e841a6704fdb5d17ab00bf5
Khale Coffran <khalecoffran@gmail.com>
2023-09-27 13:31:06 -0400

    added comments

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ function ask(questionText) {
 async function start() {
   let playAgain = "y";
   var cheated = false;
+  // while loop of the game until playAgain is set to !"y"
   while (playAgain.toLowerCase() == "y"){
     cheated = false;
+    // Asks for the range and chosen number
     console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.");
     let lowestNum = await ask("What is the lowest number in the range? ");
     let highestNum = await ask("What is the highest number in the range? ");
@@ -23,6 +25,7 @@ async function start() {
     highestNum = Number(highestNum);
     secretNumber = Number(secretNumber);
     
+    // Checks weither or not the player is cheating with setting range and secretNumber
     if (secretNumber > highestNum || secretNumber < lowestNum){
       console.log("No fair! Your trying to Cheat!");
       cheated = true;
@@ -30,17 +33,22 @@ async function start() {
       console.log("That's not a number.");
       cheated = true;
     } else {
+      // game begins
       console.log("Lets begin!");
       let numGuessed = "n";
       var tries = 1;
       tries = 1;
+      // while loop continues until the player answers that the numGuessed is = to secretNumber
       while (numGuessed.toLowerCase() != "y" && cheated == false) {
         numGuessed = await ask(`Is your Number ${Math.floor((highestNum + lowestNum) / 2)}? (Y/N)? `);
+        // if numGuessed is not secretNumber then ask if the secretNumber is higher or lower then numGuessed
         if (numGuessed.toLowerCase() == "n") {
           tries++;
+          // Checks that the prevGuess is not the current guess
           if (prevGuess != (Math.floor((highestNum + lowestNum) / 2))){
             var prevGuess = 0;
             let isNum = await ask("Is your Number higher or lower? (H/L)? ");
+            // Changes the min and max range of the algorithem to correctly change the guess
             if (isNum.toLowerCase() == "h"){
               lowestNum = Math.floor((highestNum + lowestNum) / 2);
               prevGuess = lowestNum;
@@ -49,16 +57,18 @@ async function start() {
               prevGuess = highestNum;
             }
           } else {
+            // If the player doesn't answer yes after the algorithem has reached a point where it can only give one number, end the gamse and accuse the player of cheating
             console.log(`Your number can only be ${prevGuess} baised on what you told me. You have lied, thou art a villan and cur.`);
             numGuessed = "y";
             cheated = true;
           }
         } else if (cheated == false && numGuessed.toLowerCase() == "y"){
+          // If the player has not been delared a cheater, end the game displaying the number of tries it took and the secretNumber 
           console.log(`Hurray, I got it in ${tries} tries! Your number was ${Math.floor((highestNum + lowestNum) / 2)}.`);
         }
       }
     }
-
+    // Ask player if they wish to play again
     playAgain = await ask("Would you like to play again? (Y/N) ");
 }
 process.exit();
@@ -67,6 +77,7 @@ process.exit();
 start();
 
 // ? Bugged (displays double input in terminal but reads singular has to do with readline running in both)
+// * function to chose which game the user wants to play
 // async function games(){
 // let game = await ask("Would you like to play regular or reverse? ");
 //   if (game.toLowerCase() == "regular") {
--- reverse_game.js
+++ reverse_game.js
@@ -9,25 +9,33 @@ function ask(questionText) {
 
 async function reverse(){
     let playAgain = "y";
+    // while loop of the game until playAgain is set to !"y"
     while (playAgain.toLowerCase() == "y") {
         var tries = 0;
         tries = 0;
+        // displays the rules of the game
         console.log("Let's play a game where I (computer) make up a number,\n1 - 100,\nand you (human) try to guess it.");
+        // creates a random number from 0-100 and assigns it to num
         var num = Math.floor(Math.random()*100);
+        // while loop until guess is correct
         while (guess != num) {
             var guess = await ask("What is your guess? ");
             guess = Number(guess);
             tries++;
+            // If input is invalid tell the player that it is so
             if (isNaN(guess)) {
                 console.log("That is not a number");
+            // checks if guess is higher or lower than num and displays a message conveying so.
             } else if (guess > num) {
                 console.log("That number is to high.");
             } else if (guess < num) {
                 console.log("That guess is to low.");
+            // prints victory message
             } else {
                 console.log(`Congrats you guessed it in ${tries}!`);
             }
         }
+        // Ask player if they wish to play again
         playAgain = await ask("Would you like to play again? (Y/N) ");
     }
     process.exit();
